aed2c5524b2f795a6d87264ff86a7bd49e991ad0
LemonaInc <jaxonstevens@lemonainc.com>
2017-09-10 15:45:39 -0700

    Updated Network Connection Check and reachability class

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Roxa' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Roxa
+
+pod 'JSSAlertView'
+
+
+  target 'RoxaTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'RoxaUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- Roxa/ViewController.swift
+++ Roxa/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import WebKit
+import JSSAlertView
 
 class ViewController: UIViewController, UIWebViewDelegate   {
    
@@ -18,27 +19,56 @@ class ViewController: UIViewController, UIWebViewDelegate   {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         roxaWebView.delegate = self
-
         self.roxaWebView.scalesPageToFit = false
         self.roxaWebView.isMultipleTouchEnabled = false
         
+        // http://localhost:8888/main.html
+
+    }
     
-      //  https://app.outbully.com/admin
-        let urlPath:String = "http://localhost:8888/"
-        let theURL = URLRequest(url:URL(string: urlPath)!)
+    
+    override func viewDidAppear(_ animated: Bool) {
+       
+        // Define the url to be displayed
+        let urlPath:String = "http://jaxonstevens.com"
+        // Make a URL request and set the url path
+        let roxaURL = URLRequest(url:URL(string: urlPath)!)
+        // load the url defined
+        self.roxaWebView.loadRequest(roxaURL)
         
+        // Call the loading view function
+        loadingView()
+        // Call the network check function
         
-        self.roxaWebView.loadRequest(theURL)
+    }
+    
+    // Call this function when the view is loading to display a alert view
+    func loadingView() {
+        
+        JSSAlertView().show(self, title: "Loading Roxa", noButtons: true, delay: 3)
+    }
+    
+    // Check the network call and if there is a connection proceed, if not show a warning
+    func networkCheck() {
+        
+      var networkCheckAlertView = JSSAlertView().show(self,
+            title: "Network Connection Failed",
+            text: "Please check your connection",
+            color: UIColorFromHex (0xF95C5C, alpha: 1)
+            
+            )
+          //  customIcon:("")
+
         
     }
     
     // Webview did load
     func webViewDidFinishLoad(webView: UIWebView) {
         print("webview did finish load!")
+        
+        
     }
-    
     
     // Webview did fail to load with error
     func webView(webView: UIWebView, didFailLoadWithError error: Error) {
@@ -50,17 +80,6 @@ class ViewController: UIViewController, UIWebViewDelegate   {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    // Did fail to load webview function
-    
-    /* func webView(webView: UIWebView, didFailLoadWithError error: NSError?) {
-        let myAlert = UIAlertController(title: "Alert", message: error?.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
-        self.present(myAlert, animated: true, completion: nil)
-    } */
-    
-    
-    
-
 
 }
 
